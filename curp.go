842450a3081d39f71ab582915637d15a475eb7a3
David Torres <dabiddo@gmail.com>
2019-10-29 22:10:54 -0700

    changed day of birth from separated strings to ISO layout, added mini function to search for born state and gender

--- curp.go
+++ curp.go
@@ -1,6 +1,16 @@
 package curp
 
-func Generate(firstName string, middleName string, paternalName string, maternalName string, yearOfBirth string, monthOfBirth string, dayOfBirth string)string{
+import (
+	"strconv"
+	"time"
+)
+
+const (
+	layoutISO = "2006-01-02"
+	layOutYear = "06"
+)
+
+func Generate(firstName string, middleName string, paternalName string, maternalName string, dob string, gender string, myState string)string{
 
 	//Step 1: Get First Letter and First Vowel from the Patermal Name
 	//Step 2: Second letter is the first vowel from the paternal name
@@ -11,14 +21,32 @@ func Generate(firstName string, middleName string, paternalName string, maternal
 	thirdLetter := string(maternalName[0])
 	fourthLetter := string(firstName[0])
 	alphabeticCode := firstLetter + secondLetter + thirdLetter + fourthLetter
-
+	//fmt.Println(rune('H'))
 	//For the Digit Codes, we take the las 2 digits of the Year of Birth
 	// The month of birth in digit code
 	//the day of birth in digit code
-	year := string(yearOfBirth[2])+string(yearOfBirth[3])
-	birthCode := year + monthOfBirth + dayOfBirth
+	dateString,_ := time.Parse(layoutISO, dob)
 
-	generatedCurp := alphabeticCode+birthCode
+	year:=  strconv.Itoa(dateString.Year())
+
+	intMonth,month := int(dateString.Month()), ""
+
+	if intMonth < 10 {
+		month = "0" + strconv.Itoa(intMonth)
+	}else {
+		month = strconv.Itoa(intMonth)
+	}
+
+	day := strconv.Itoa(dateString.Day())
+
+	birthCode := year[2:4] + month + day
+
+	//We add the Gender H/M
+	// we look for the born state
+	stateKey := bornState(myState)
+
+
+	generatedCurp := alphabeticCode+birthCode+gender+stateKey
 	return generatedCurp
 }
 
@@ -38,4 +66,20 @@ func firstVowel(name string)string  {
 		}
 	}
 	return vowel
+}
+
+func bornState(myState string)string  {
+	var states  = map[string]string{
+		"BC":"BAJA CALIFORNIA",
+		"MC": "MEXICO",
+		"SN" : "SINALOA",
+	}
+
+	for k,v := range states  {
+		if v == myState {
+			return k
+		}
+	}
+	return "NE" //If its not any known state, we have to assume this person was born in another country
+
 }
--- curp_test.go
+++ curp_test.go
@@ -6,9 +6,9 @@ import (
 
 func TestCurp(t *testing.T) {
 
-	myCurp := Generate("LUIS", "RAUL", "BELLO", "MENA", "1992","03","13")
+	myCurp := Generate("LUIS", "RAUL", "BELLO", "MENA", "1992-03-13","H","MEXICO")
 
-	if  myCurp != "BEML920313"{
+	if  myCurp != "BEML920313HMC"{
 		t.Error("CURP is Incorrect",myCurp)
 	}
 }
